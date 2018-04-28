f2d7413c1ddf7f569ee3079eaba09eefca8d0ee4
Thilo Molitor <thilo@eightysoft.de>
2018-04-29 00:56:43 +0200

    Add timer functions to base routing class

--- routing/base.py
+++ routing/base.py
@@ -1,6 +1,9 @@
+import uuid
+from datetime import datetime
+from operator import itemgetter
+from sortedcontainers import SortedList
 from queue import Queue, Empty, Full
-from threading import Thread
-from threading import Event
+from threading import Thread, Event, RLock, Condition
 import logging
 logger = logging.getLogger(__name__)
 
@@ -10,21 +13,28 @@ from networking import Connection, Message
 class Router(object):
     stopped = Event()
     
+    # public interface
     def __init__(self, node_id, queue):
         self.queue = queue
         self.node_id = node_id
         self.connections = {}
         self.subscriptions = {}
-        self.publish_ttl = 0
+        self.timers = SortedList(key=itemgetter('timeout'))
+        self.timers_condition = Condition()
         self.routing_thread = Thread(name="local::"+self.node_id+"::_routing", target=self._routing)
         self.routing_thread.start()
+        self.timers_thread = Thread(name="local::"+self.node_id+"::_timers", target=self._timers)
+        self.timers_thread.start()
     
     def stop(self):
         for peer_id, con in self.connections.items():
             con.terminate()
         Router.stopped.set()
+        with self.timers_condition:
+            self.timers_condition.notify()  # stop wait in timers thread
         self.queue.put({})      #empty put to wake up routing thread after Router.stopped is set to True
         self.routing_thread.join()
+        self.timers_thread.join()
     
     def publish(self, channel, data):
         logger.info("Publishing data on channel '%s'..." % str(channel))
@@ -42,6 +52,7 @@ class Router(object):
             "callback": callback
         })
     
+    # internal methods
     def _route_data(self, msg, incoming_connection=None):
         pass
     
@@ -74,20 +85,60 @@ class Router(object):
     def _publish_command(self, command):
         pass    # this command has no common implementation that could be used by child classes
     
+    def _add_timer(self, timeout, command):
+        timer_id = str(uuid.uuid4())
+        with self.timers_condition:
+            self.timers.add({"timeout": datetime.now().timestamp() + timeout, "id": timer_id, "command": command})
+            self.timers_condition.notify()      # notify timers thread of changes
+        return timer_id
+    
+    def _abort_timer(self, timer_id):
+        with self.timers_condition:
+            # filter out this timer id
+            self.timers = SortedList(iterable=[entry for entry in self.timers if entry["id"] != timer_id], key=itemgetter('timeout'))
+            self.timers_condition.notify()      # notify timers thread of changes
+    
+    def _timers(self):
+        logger.debug("timer thread started...");
+        while not Router.stopped.isSet():
+            # wait for nex timer or for 8 seconds if no timer is currently present
+            with self.timers_condition:
+                if len(self.timers):
+                    logger.debug("next timer event in %s seconds" % str(self.timers[0]["timeout"] - datetime.now().timestamp()))
+                    self.timers_condition.wait(self.timers[0]["timeout"] - datetime.now().timestamp())
+                else:
+                    logger.debug("next timer event in %s seconds" % str(60))
+                    self.timers_condition.wait(60)   # this gets aborted as soon as a timer is added to the list
+            # stop here if the router is stopped
+            if Router.stopped.isSet():
+                break
+            # execute all now expired timers
+            expired = []
+            now = datetime.now().timestamp()
+            with self.timers_condition:
+                for timer in self.timers:
+                    if timer["timeout"] <= now:
+                        logger.debug("timer %s adding command %s to queue" % (str(timer["id"]), str(timer["command"])))
+                        self.queue.put(timer["command"])
+                        expired.append(timer["id"])
+                    else:
+                        break       # we are sorted, so stop here
+                # clean up fired timers afterwards (that can be less than are expired by NOW!)
+                self.timers = SortedList(iterable=[entry for entry in self.timers if entry["id"] not in expired], key=itemgetter('timeout'))
+        logger.debug("timer thread stopped...")
+    
     def _routing(self):
         logger.debug("routing thread started...");
         while not Router.stopped.isSet():
             try:
-                command = self.queue.get(1)      #1 second timeout
-                if Router.stopped.isSet():
+                command = self.queue.get(1)     #1 second timeout
+                if Router.stopped.isSet():      # don't process anything here if we are stopped
                     break
             except Empty as err:
                 logger.debug("routing queue empty");
                 continue
-            if Router.stopped.isSet():  # don't process anything here if we are stopped
-                break
             logger.debug("got routing command: %s" % command["command"])
-            func = "_"+command["command"]+"_command"
+            func = "_"+command["command"]+"_command"        # this is the method name we have to call on self to process the command
             if hasattr(self, func):
                 getattr(self, func)(command)
             else:
