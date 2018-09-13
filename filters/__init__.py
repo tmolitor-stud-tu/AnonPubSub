1aa3722eee632240d7a61e1768f5765e9033a5d0
Thilo Molitor <thilo@eightysoft.de>
2018-09-13 06:47:13 +0200

    Add new router_command_incoming for imports

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,1 +1,1 @@
-from .filters import load, update_attributes, gui_command_incoming, gui_command_completed, gui_event_outgoing, covert_msg_incoming, covert_msg_outgoing, msg_incoming, msg_outgoing
+from .filters import load, update_attributes, gui_command_incoming, gui_command_completed, gui_event_outgoing, router_command_incoming, covert_msg_incoming, covert_msg_outgoing, msg_incoming, msg_outgoing
